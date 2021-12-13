36e9b468c623e9d806d5e68b9af3386159bcbd1b
ivanchik239 <ivan-obraztsov@ya.ru>
2021-12-13 17:35:56 +0300

    gh pages

--- src/api_keys.js
+++ src/api_keys.js
@@ -1,4 +1,4 @@
 export const FOOD_API_KEY_2 = 'de6f794a52fc46d388b7ae25f4c1ebcd';
 export const FOOD_API_KEY_4 = '5b7594bb9ce44dab92220481200c5f4c';
 export const FOOD_API_KEY = '99ae868dd60c44e29dbcd511139da490';
-export const FOOD_API_KEY_3 = '0b45f1c2cf71476f8b2fe0985f232e93'
+export const FOOD_API_KEY_3 = 'e7f0d76776c1442aa299aae27c12b2e3';
